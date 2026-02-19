dda810b8f78e885405449d2b064227042856b791
Frank00067 <f.nkurunziz2@alustudent.com>
2026-02-19 22:23:41 +0200

    Correct SQL group by clause and import formatting
    Fix typo in SQL query and import statement.

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+ from flask import Flask, jsonify
 from flask_cors import CORS
 from sqlalchemy import create_engine, text
 import os
@@ -33,6 +33,7 @@ def get_summary():
     })
 
 # API endpoint to return the spatial boundaries of all zones for map visualization
+>>>>>>> 7e04deab0ef702b9d31bc91a76f9e6530abf0dac
 @app.route('/api/zones', methods=['GET'])
 def get_zones():
     query = text("select location_id, geometry from spatial_zones")
@@ -91,14 +92,17 @@ def get_top_locations():
 def get_trends():
     query = text("""
         select
+<<<<<<< HEAD
             date(pinkup_datetime) as trip_date,
             count(*) as trip_count
         from trips
         goup by trip_date
+=======
             date(pickup_datetime) as trip_date,
             count(*) as trip_count
         from trips
         group by trip_date
+>>>>>>> 7e04deab0ef702b9d31bc91a76f9e6530abf0dac
         order by trip_date;
     """)
     with engine.connect() as conn:
